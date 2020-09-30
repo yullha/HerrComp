603ba07d3ca4e48b154c1e54413919c41092dfb2
yevillalbaa <yevillalbaa@unal.edu.co>
2020-09-30 07:29:46 +0000

    Actualized

--- 2020-09-09-Makefiles/plot.py
+++ 2020-09-09-Makefiles/plot.py
@@ -8,6 +8,6 @@ ax.plot(datax, datay, 'r-o', label="Data")
 ax.legend()
 ax.set_xlabel("Número de terminos")
 ax.set_ylabel("Delta %")
-ax.set_title("Title")
+#ax.set_title("Title")
 
 fig.savefig("fig.pdf")
